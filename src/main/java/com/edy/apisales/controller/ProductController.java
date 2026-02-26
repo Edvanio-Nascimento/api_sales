f52405b70f7c487ba6139c7a05831710511b2e55
Edy Nascimento <nascoliedy@gmail.com>
2026-02-26 17:18:18 -0400

    Feat: Inserindo a configuração de cors no controller

--- src/main/java/com/edy/apisales/controller/ProductController.java
+++ src/main/java/com/edy/apisales/controller/ProductController.java
@@ -5,14 +5,12 @@ import com.edy.apisales.dto.product.ProductSummary;
 import com.edy.apisales.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("api/v1/products")
 public class ProductController {
