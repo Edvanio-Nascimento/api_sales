f3531947b42e305806a4400a94ba8385d1e64b56
Edy Nascimento <nascoliedy@gmail.com>
2026-03-30 12:41:42 -0400

    Feat: Criando a entidade de ProductsModel

---
+++ src/main/java/com/github/edy/course_api_sales/model/ProductModel.java
@@ -0,0 +1,58 @@
+package com.github.edy.course_api_sales.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Entity @Table(name = "tb_products")
+public class ProductModel implements Serializable {
+
+    @Serial
+    private  static  final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "Binary(16)")
+    private UUID id;
+
+    @Column(nullable = false, length = 30, unique = true)
+    private String sku;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, columnDefinition = "text")
+    private String description;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    private Integer stock = 0;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
