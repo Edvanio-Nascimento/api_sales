80cc518234f4268247500f07af908138d57d6300
Edy Nascimento <nascoliedy@gmail.com>
2026-03-30 12:11:18 -0400

    Iniciando o projeto

---
+++ src/main/java/com/github/edy/course_api_sales/CourseApiSalesApplication.java
@@ -0,0 +1,13 @@
+package com.github.edy.course_api_sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/edy/course_api_sales/CourseApiSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.edy.course_api_sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
